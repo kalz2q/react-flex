764e8a73f10960789a63280806f91d7c75671f54
kalz2q <katsu95i@gmail.com>
2022-07-27 19:08:08 +0900

    MMM

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
       <header>Header</header>
       <div className="container">
         <main>Main</main>
+        <nav>Nav</nav>
         <aside>Aside</aside>
       </div>
       <footer>Footer</footer>
